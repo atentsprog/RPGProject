e076d6d8ef20cafbc29cc585beb14cf6eb388921
hktrollgames <hktrollgames@gmail.com>
2021-08-17 16:19:55 +0900

    플레이어 이동 애니메이션 하게함

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,14 +14,16 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
+    [SerializeField] float playerSpeed = 5.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
     Transform cameraTransform;
+    Animator animator;
     private void Awake()
     {
         cameraTransform = Camera.main.transform;
-        controller = gameObject.GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
         jumpAction = playerInput.actions["Jump"];
         moveAction = playerInput.actions["Move"];
     }
@@ -40,10 +42,6 @@ public class PlayerController : MonoBehaviour
         move.y = 0;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        //if (move != Vector3.zero)
-        //{
-        //    gameObject.transform.forward = move;
-        //}
 
         // Changes the height position of the player..
         if (jumpAction.triggered && groundedPlayer)
@@ -57,6 +55,21 @@ public class PlayerController : MonoBehaviour
         // 회전.
         Quaternion targetRotation = Quaternion.Euler(0, cameraTransform.rotation.eulerAngles.y, 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+
+        // 애니메이션
+        if (move.sqrMagnitude > 0)
+        {
+            float forwardDegree = transform.forward.VectorToDegree();
+            float moveDegree = move.VectorToDegree();
+            float dirRadian = (moveDegree - forwardDegree + 90) * Mathf.PI / 180; //라디안값
+            Vector3 dir;
+            dir.x = Mathf.Cos(dirRadian);// 
+            dir.z = Mathf.Sin(dirRadian);//
+            animator.SetFloat("DirX", dir.x);
+            animator.SetFloat("DirY", dir.z);
+        }
+        animator.SetFloat("Speed", move.sqrMagnitude);
     }
     public float rotationSpeed = 5;
 }
