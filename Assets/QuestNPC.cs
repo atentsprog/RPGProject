5c7b4cf57ebc881c055f13117e2b189d6a44267b
hktrollgames <hktrollgames@gmail.com>
2021-08-18 21:30:37 +0900

    퀘스트 수락 키 작업함

--- Assets/QuestNPC.cs
+++ Assets/QuestNPC.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class QuestNPC : MonoBehaviour
 {
+    public InputAction questAcceptKey;
+
+    private void Awake() =>questAcceptKey.performed += QuestAcceptKey_performed;
+    private void QuestAcceptKey_performed(InputAction.CallbackContext obj)
+    {
+        print("퀘스트 목록 UI푯기 하자.");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        print(other);
-        TalkAlertUI.Instance.ShowText(@"모험자야 멈춰봐!
-할말이 있어(Q)");
+        if (other.CompareTag("Player") == false)
+            return;
+        print(other); 
+        questAcceptKey.Enable();
+        TalkAlertUI.Instance.ShowText("모험자야 멈춰봐!\n할말이 있어(Q)");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") == false)
+            return;
+        questAcceptKey.Disable();
     }
 }
