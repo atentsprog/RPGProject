c2c15055912ffa4cc16227462f14da94aaf775bd
hktrollgames <hktrollgames@gmail.com>
2021-08-20 15:51:10 +0900

    퀘스트 데이터 작업중

--- Assets/QuestListUI.cs
+++ Assets/QuestListUI.cs
@@ -3,9 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+/*
+퀘스트 이름 : questTitle
+상세내용 : detailExplain
+목표 Type : enum QuestType
+ KillMonster,      // 몬스터 처치.
+ GoToDestination,  // 목적지 도착
+ ItemCollection, // 아이템 수집
+ 
+GoalCount : 
+	몬스터 처치시는 몬스터 처치수
+	아이템 수집시는 아이템 수집수,
+	
+class Reward
+{
+ItemID
+Count 
+}*/
+
+public enum QuestType
+{
+    KillMonster,      // 몬스터 처치.
+    GoToDestination,  // 목적지 도착
+    ItemCollection, // 아이템 수집
+}
+[System.Serializable]
+public class RewardInfo
+{
+    public int itemID;
+    public int count;
+}
+
+[System.Serializable]
+public class QuestInfo
+{
+    public string questTitle;
+    [TextArea]
+    public string detailExplain;
+    public QuestType questType;
+
+    /// <summary>
+    /// 몬스터 처치시는 몬스터 ID
+    /// 아이템 수집시는 아이템 ID,</summary>
+    public int GoalId;
+    /// <summary>
+    /// 몬스터 처치시는 몬스터 처치수
+    /// 아이템 수집시는 아이템 수집수,</summary>
+    public int GoalCount;
+
+    public List<RewardInfo> rewards;
+}
 public class QuestListUI : Singleton<QuestListUI>
 {
     CanvasGroup canvasGroup;
+    public List<QuestInfo> quests;
     void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
@@ -18,20 +69,3 @@ public class QuestListUI : Singleton<QuestListUI>
         canvasGroup.DOFade(1, 0.5f);
     }
 }
-
-/*
-퀘스트 이름 : questTitle
-상세내용 : detailExplain
-목표 Type : enum QuestType
- KillMonster,      // 몬스터 처치.
- GoToDestination,  // 목적지 도착
- ItemCollection, // 아이템 수집
- 
-Count : 
-	몬스터 처치시는 몬스터 처치수
-	아이템 수집시는 아이템 수집수,
-	
-class Reward	
-ItemID
-Count
-*/
