81dfc28f32964b5a55b4fbbfc361905342e5752c
hktrollgames <hktrollgames@gmail.com>
2021-08-23 14:00:29 +0900

    상점 UI작업중

---
+++ Assets/TextButtonBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class TextButtonBox : MonoBehaviour
+{
+    public Text text;
+
+    public void LinkComponent()
+    {
+        text = GetComponentInChildren<Text>(true);
+    }
+
+    public void Init(string str)
+    {
+        text.text = str;
+    }
+}
