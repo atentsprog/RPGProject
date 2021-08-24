48992429b2e371b0d19cefb41a1b40f872461c42
hktrollgames <hktrollgames@gmail.com>
2021-08-24 17:54:59 +0900

    샵 작업중

--- Assets/ShopUI.cs
+++ Assets/ShopUI.cs
@@ -3,13 +3,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopUI : Singleton<ShopUI>
 {
     CanvasGroup canvasGroup;
+    GameObject shopMenuGo;
+    GameObject subCategoryGo;
+    Text guideText;
+
     void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
+        shopMenuGo = transform.Find("ShopMenu").gameObject;
+        subCategoryGo = transform.Find("SubCategory").gameObject;
+
+        guideText = transform.Find("GuideUI/GuideText").GetComponent<Text>();
     }
     private void OnEnable()
     {
@@ -19,12 +28,26 @@ public class ShopUI : Singleton<ShopUI>
     {
         StageManager.GameState = GameStateType.Play;
     }
+    public float speechSpeed = 20;
     internal void ShowUI()
     {
         gameObject.SetActive(true);
         canvasGroup.alpha = 0;
         canvasGroup.DOFade(1, 0.5f).SetUpdate(true);
+
+        shopMenuGo.SetActive(true);
+        subCategoryGo.SetActive(false);
+
+        SetGuideText("무엇을 하시겠습니까?");
     }
+
+    private void SetGuideText(string showText)
+    {
+        guideText.text = "";
+        guideText.DOKill();
+        guideText.DOText(showText, showText.VisibleTextLength() / speechSpeed);
+    }
+
     private void CloseUI()
     {
         canvasGroup.DOFade(0, 0.5f).SetUpdate(true)
