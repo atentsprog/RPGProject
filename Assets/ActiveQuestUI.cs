c8fd9e62061eee87c0b975a14acc2f11f7a74aef
hktrollgames <hktrollgames@gmail.com>
2021-08-25 20:24:53 +0900

    퀘스트 목록 새로고침될때 중복으로 생성되는 버그 수정

--- Assets/ActiveQuestUI.cs
+++ Assets/ActiveQuestUI.cs
@@ -8,24 +8,26 @@ public class ActiveQuestUI : Singleton<ActiveQuestUI>
     void Awake()
     {
         baseItem = transform.Find("BG/Item").GetComponent<TextButtonBox>();
+        baseItem.LinkComponent();
     }
      
     private void Start()
     {
         RefreshQuestList();
     }
-
+    public List<GameObject> questBoxs = new List<GameObject>();
     public void RefreshQuestList()
     {
         var activeIds = UserData.Instance.questData.data.acceptIds;
         var activeQuests = ItemDB.Instance.GetQuestInfo(activeIds);
-
-        //baseItem.LinkComponent();
+        questBoxs.ForEach(x => Destroy(x));
+        questBoxs.Clear();
         baseItem.gameObject.SetActive(true);
         foreach (var item in activeQuests)
         {
             var newItem = Instantiate(baseItem, baseItem.transform.parent);
             newItem.text.text = item.questTitle;
+            questBoxs.Add(newItem.gameObject);
         }
         baseItem.gameObject.SetActive(false);
     }
