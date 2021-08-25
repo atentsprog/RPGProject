8ed6e862a76e790139a8d50196be0babf8aea7ac
hktrollgames <hktrollgames@gmail.com>
2021-08-25 21:15:33 +0900

    인벤토리 작업중

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class InventoryUI : MonoBehaviour
+public class InventoryUI : Singleton<InventoryUI>
 {
     // Start is called before the first frame update
     void Awake()
@@ -15,36 +15,35 @@ public class InventoryUI : MonoBehaviour
             categoryBox.Add(transform.Find($"Left/CategoryBox{i}").GetComponent<TextButtonBox>());
         }
 
-        categoryBox[0].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Weapon));
-        categoryBox[1].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Armor));
-        categoryBox[2].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Accesory));
-        categoryBox[3].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Consume));
-        categoryBox[4].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Material));
-        categoryBox[5].button.onClick.AddListener(()=>ShowItemCategory(ItemType.Etc));
+        categoryBox[0].button.onClick.AddListener(() => ShowItemCategory(ItemType.Weapon));
+        categoryBox[1].button.onClick.AddListener(() => ShowItemCategory(ItemType.Armor));
+        categoryBox[2].button.onClick.AddListener(() => ShowItemCategory(ItemType.Accesory));
+        categoryBox[3].button.onClick.AddListener(() => ShowItemCategory(ItemType.Consume));
+        categoryBox[4].button.onClick.AddListener(() => ShowItemCategory(ItemType.Material));
+        categoryBox[5].button.onClick.AddListener(() => ShowItemCategory(ItemType.Etc));
     }
 
     List<GameObject> inventoryGos = new List<GameObject>();
 
-    TextButtonBox shopItemListBoxBase;
+    ItemBox baseBox;
     private void ShowItemCategory(ItemType itemType)
     {
-
         // 리스트를 표시하자.
         List<InventoryItemInfo> showItemList = UserData.Instance.GetItems(itemType);
 
         inventoryGos.ForEach(x => Destroy(x));
         inventoryGos.Clear();
 
-        shopItemListBoxBase.gameObject.SetActive(true);
+        baseBox.gameObject.SetActive(true);
         foreach (var item in showItemList)
         {
-            ShopItemListBox newBox = Instantiate(shopItemListBoxBase, shopItemListBoxBase.transform.parent);
-            newBox.Init(item.ItemInfo);
+            ItemBox newBox = Instantiate(baseBox, baseBox.transform.parent);
+            newBox.Init(item);
             inventoryGos.Add(newBox.gameObject);
 
             newBox.button.onClick.AddListener(() => OnClick(item));
         }
-        shopItemListBoxBase.gameObject.SetActive(false);
+        baseBox.gameObject.SetActive(false);
 
         void OnClick(InventoryItemInfo item)
         {
@@ -61,9 +60,8 @@ public class InventoryUI : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowUI()
     {
-        
+        ShowItemCategory(ItemType.Weapon);
     }
 }
---
+++ Assets/ItemBox.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ItemBox : MonoBehaviour
+{
+    public Image icon;
+    public Button button;
+    public Text count;
+    public GameObject activeGo;
+    public void LinkComponent()
+    {
+        if(icon == null)
+            icon = transform.Find("Icon").GetComponent<Image>();
+        if (icon == null)
+            icon = GetComponentInChildren<Image>();
+
+        if (button == null)
+            button = GetComponent<Button>();
+
+        if (count == null)
+            count = GetComponentInChildren<Text>();
+
+        if (activeGo == null)
+            activeGo = transform.Find("ActiveState").gameObject;
+    }
+
+    internal void Init(InventoryItemInfo item)
+    {
+        icon.sprite = item.ItemInfo.Sprite;
+        count.text = item.count.ToString();
+    }
+}
