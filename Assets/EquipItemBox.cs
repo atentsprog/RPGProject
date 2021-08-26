1c4f9b878842c762aa61906a88643a93dea557c0
hktrollgames <hktrollgames@gmail.com>
2021-08-26 20:13:29 +0900

    EquipItemBox 생성

---
+++ Assets/EquipItemBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ItemBox))]
+public class EquipItemBox : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EquipUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipUI : MonoBehaviour
+{
+    EquipItemBox baseBox;
+
+    void Awake()
+    {
+        baseBox = GetComponentInChildren<EquipItemBox>(true);
+        baseBox.gameObject.SetActive(true);
+        for (int i = 0; i < 8; i++)
+        {
+            Transform parent;
+            //parent = transform.Find(i < 4 ? "EqupItem/Right" : "EqupItem/Left");
+            if (i < 4)
+                parent = transform.Find("EqupItem/Right");
+            else
+                parent = transform.Find("EqupItem/Left");
+
+            EquipItemBox newBox = Instantiate(baseBox, parent);
+            //newBox.Init(item);
+            //newBox.button.onClick.AddListener(() => OnClick(item));
+        }
+        baseBox.gameObject.SetActive(false);
+    }
+}
