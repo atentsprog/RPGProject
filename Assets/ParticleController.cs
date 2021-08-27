894f1cc5ce1670e776d82ebd8aff262384782a71
hktrollgames <hktrollgames@gmail.com>
2021-08-27 19:10:02 +0900

    재생끝난 파티클 삭제

---
+++ Assets/ParticleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ps.isPlaying)
+            return;
+
+        /// 파티클 더이상 안나옴
+        Destroy(gameObject);
+    }
+}
