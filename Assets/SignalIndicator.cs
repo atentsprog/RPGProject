9698eb47258f83bdcb1cb30605f4fc9078b2383d
hktrollgames <hktrollgames@gmail.com>
2021-08-27 16:06:45 +0900

    미니맵 핑 표시 추가중

---
+++ Assets/SignalIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalIndicator : MonoBehaviour
+{
+    Collider terrainCollider;
+    private void Awake()
+    {
+        terrainCollider = Terrain.activeTerrain.GetComponent<Collider>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftAlt))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(terrainCollider.Raycast(ray, out RaycastHit hit, float.MaxValue))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
