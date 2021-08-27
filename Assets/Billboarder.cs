e6b4e40cfa2f2fc6b2464c76fedd2aa28a6122e2
hktrollgames <hktrollgames@gmail.com>
2021-08-27 16:47:28 +0900

    빌보딩 이펙트 생성

---
+++ Assets/Billboarder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboarder : MonoBehaviour
+{
+    public bool yOnly;
+    Transform cameraTr;
+    void Start()
+    {
+        cameraTr = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (yOnly)
+        {
+            transform.LookAt(cameraTr);
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.x = 0;
+            rotation.z = 0;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        else
+            transform.forward = cameraTr.forward;
+    }
+}
