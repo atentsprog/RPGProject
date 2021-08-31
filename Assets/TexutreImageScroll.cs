d2ed42286b47ca952b42b455f4ee6b703958c345
hktrollgames <hktrollgames@gmail.com>
2021-08-31 21:02:27 +0900

    폭포 텍스쳐 offset애니메이션 함

---
+++ Assets/TexutreImageScroll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TexutreImageScroll : MonoBehaviour
+{
+    float curX = 0;
+    float curY = 0;
+    public Vector2 amount;
+    new MeshRenderer renderer;
+    private void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        curX += amount.x;
+        curY += amount.y;
+        renderer.sharedMaterial.mainTextureOffset = new Vector2(curX, curY);
+    }
+}
