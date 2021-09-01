c51b90d847ac79d1cc68d0d4a2726c3ef6cb8fc4
hktrollgames <hktrollgames@gmail.com>
2021-09-01 18:04:22 +0900

    데칼 화살 불빛 미세하게 흔듬

---
+++ Assets/NoiseTranslate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseTranslate : MonoBehaviour
+{
+    public Vector3 noise = new Vector3(0.05f, 0.05f, 0.05f);
+    public float translateTime = 0.1f;
+    IEnumerator Start()
+    {
+        Vector3 originalPos = transform.position;
+        while(true)
+        {
+            transform.position = originalPos + new Vector3(
+                Random.Range(-noise.x, noise.x),
+                Random.Range(-noise.y, noise.y),
+                Random.Range(-noise.z, noise.z));                
+
+            yield return new WaitForSeconds(Random.Range(0, translateTime));
+        }
+    }
+}
---
+++ Assets/RePositionY.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RePositionY : MonoBehaviour
+{
+    public float y = 0.5f;
+    void Start()
+    {
+        transform.Translate(0, y, 0, Space.World);
+    }
+}
