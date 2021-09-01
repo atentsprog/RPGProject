6495e5483539f6142f243045bb2cb3a612b11ec5
hktrollgames <hktrollgames@gmail.com>
2021-09-01 14:49:41 +0900

    화살 불빛 흔들리게함

---
+++ Assets/NoiseTranslate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseTranslate : MonoBehaviour
+{
+    public Vector3 move = new Vector3(0.1f, 0.1f, 0.1f);
+    public float randomTransitionTime = 0.1f;
+    IEnumerator Start()
+    {
+        Vector3 originalPosition = transform.position;
+        while(true)
+        {
+            transform.position = originalPosition + new Vector3(
+                Random.Range(-move.x, move.x),
+                Random.Range(-move.y, move.y),
+                Random.Range(-move.z, move.z));
+
+            yield return new WaitForSeconds(Random.Range(0, randomTransitionTime));
+        }
+    }
+}
