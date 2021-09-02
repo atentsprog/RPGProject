b69ed9fdda8ec2e83d691103179eac75dd068e80
hktrollgames <hktrollgames@gmail.com>
2021-09-02 17:06:05 +0900

    불빛 범위 조정하는 스크립트 추가

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -1,18 +1,30 @@
 ﻿using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    private float duration = 0.5f;
-    private float delay = 4;
-
-    void Start()
+    public float minScale = 0.5f;
+    public float maxScale = 1.5f;
+    public float duration = 0.5f;
+    IEnumerator Start()
     {
         Light light = GetComponent<Light>();
-        DOTween.To(() => light.intensity, x => light.intensity = x, 0, duration)
-            .SetDelay(delay)
-            .OnComplete( () => light.enabled = false);
+
+        float originalRange = light.range;
+        TweenerCore<float, float, FloatOptions> tween = null;
+        while (true)
+        {
+            float _duratio =  Random.Range(0, duration);
+            tween = DOTween.To(() => light.range, x => light.range = x, originalRange * Random.Range(minScale, maxScale)
+                , _duratio)
+                .SetLink(gameObject);
+
+            yield return new WaitForSeconds(_duratio);
+            tween.Kill();
+        }
     }
 }
