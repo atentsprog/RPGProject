6bc4c16a2406e12a0af47400a80ab5945a584fac
hktrollgames <hktrollgames@gmail.com>
2021-09-03 03:32:50 +0900

    책넘기는 애니메이션이 점점 밑으로 내려가는 현상 보완

--- Assets/RawImageAnimation.cs
+++ Assets/RawImageAnimation.cs
@@ -5,60 +5,26 @@ using UnityEngine.UI;
 
 public class RawImageAnimation : MonoBehaviour
 {
-    //public int xCount = 4;
-    //public int yCount = 5;
-    //public float speed = 0.1f;
-
-    //RawImage rawImage;
-    //IEnumerator Start()
-    //{
-    //rawImage = GetComponent<RawImage>();
-    //float width = 1f / xCount; // 0.25
-    //float height = 1f / yCount;
-    //var rect = rawImage.uvRect;
-    //rect.width = width;
-    //rect.height = height;
-    //rect.x = rect.y = 0;
-    //rawImage.uvRect = rect;
-
-    //WaitForSeconds waitSecond = new WaitForSeconds(speed);
-
-    //int number = 1;
-    //while (true)
-    //{
-    //    yield return waitSecond;
-    //    rect.x = width * number;
-    //    rect.y = height * number;
-    //    //xCount
-    //    //yCount
-
-    //    rawImage.uvRect = rect;
-    //    number++;
-    //}
-    //}
-
     [SerializeField] float waitSecond = 0.1f;
-    float xCount = 4;
-    float yCount = 5;
+    public int xCount = 4;
+    public int yCount = 5;
+    public Vector2 offset;
     IEnumerator Start()
     {
         RawImage rawImage = GetComponent<RawImage>();
         var rect = rawImage.uvRect;
-        float width = 1f / xCount; // 0.25
-        float height = 1f / yCount;
+        float width = (1f - offset.x) / xCount; 
+        float height = (1f - offset.y) / yCount;
+        int number = 1;
+        int totalCount = xCount * yCount;
+        WaitForSeconds waitForSecond = new WaitForSeconds(waitSecond);
         while (true)
         {
-            for (int y = 0; y < yCount; y++)
-            {
-                rect.y = 1 - ((y + 1) * height);
-                for (int x = 0; x < xCount; x++)
-                {
-                    rect.x = x * width;
-                    //print($"rect : {rect}");
-                    rawImage.uvRect = rect;
-                    yield return new WaitForSeconds(waitSecond);
-                }
-            }
-        }
+            int numberPerLine = number++ % totalCount;
+            rect.x = offset.x + width * (numberPerLine % xCount);
+            rect.y = offset.y + height * (numberPerLine / xCount);
+            rawImage.uvRect = rect;
+            yield return waitForSecond;
+        } 
     }
 }
